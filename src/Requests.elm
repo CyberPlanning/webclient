1172b50be1325958af4a9de7d26c714a40ff054a
Hedroed <nathan.rydin@gmail.com>
2017-10-07 22:57:46 +0200

    Add planning view and group selector

---
+++ src/Requests.elm
@@ -0,0 +1,57 @@
+module Requests exposing (..)
+
+import GraphQl
+import Json.Encode as Encode
+import Json.Decode as Decode exposing (Decoder, field)
+import Types exposing (Query, Event, decodeQuery)
+import Date
+import Http exposing (Error)
+
+type alias Group =
+    { name : String
+    , slug : String
+    }
+
+type Msg
+    = GraphQlMsg (Result Error Query)
+    | SetDate Date.Date
+    | SetGroup String
+
+
+planningRequest : GraphQl.Value GraphQl.Root
+planningRequest =
+  GraphQl.named "day_planning"
+    [ GraphQl.field "planning"
+      |> GraphQl.withArgument "fromDate" (GraphQl.variable "from")
+      |> GraphQl.withArgument "toDate" (GraphQl.variable "to")
+      |> GraphQl.withArgument "affiliationGroups" (GraphQl.variable "grs")
+      |> GraphQl.withSelectors
+        [ GraphQl.field "events"
+          |> GraphQl.withSelectors
+            [ GraphQl.field "title"
+            , GraphQl.field "startDate"
+            , GraphQl.field "endDate"
+            , GraphQl.field "classrooms"
+            , GraphQl.field "teachers"
+            , GraphQl.field "groups"
+            ]
+        ]
+    ]
+    |> GraphQl.withVariable "from" "DateTime!"
+    |> GraphQl.withVariable "to" "DateTime!"
+    |> GraphQl.withVariable "grs" "[String]"
+
+
+baseRequest : GraphQl.Value GraphQl.Root -> Decoder a -> GraphQl.Request a
+baseRequest =
+  GraphQl.query "http://ensibs.planningiut.fr/graphql/"
+
+
+sendRequest : String -> String -> List String -> Cmd Msg
+sendRequest from to groups =
+  baseRequest planningRequest decodeQuery
+    |> GraphQl.addVariables [ ("from", Encode.string from)
+                            , ("to", Encode.string to)
+                            , ("grs", Encode.list (List.map Encode.string groups))
+                            ]
+    |> GraphQl.send GraphQlMsg
---
+++ src/Types.elm
@@ -0,0 +1,46 @@
+module Types exposing (..)
+
+import Json.Decode as Decode exposing (Decoder, field, maybe, int, string)
+
+
+type alias Planning =
+    { events : (List Event)
+    }
+
+
+type alias Event =
+    { title : String
+    , startDate : String
+    , endDate : String
+    , classrooms : (List String)
+    , teachers : (List String)
+    , groups : (List String)
+    }
+
+
+type alias Query =
+    { planning : Planning
+    }
+
+
+decodePlanning : Decoder Planning
+decodePlanning =
+  Decode.map Planning
+    (field "events" (Decode.list decodeEvent))
+
+
+decodeEvent : Decoder Event
+decodeEvent =
+  Decode.map6 Event
+    (field "title" string)
+    (field "startDate" string)
+    (field "endDate" string)
+    (field "classrooms" (Decode.list string))
+    (field "teachers" (Decode.list string))
+    (field "groups" (Decode.list string))
+
+
+decodeQuery : Decoder Query
+decodeQuery =
+    Decode.map Query
+        (field "planning" decodePlanning)
