12e5f239cfb50c8c6880bac82490923377c965a7
Hedroed <nathan.rydin@gmail.com>
2019-03-13 09:23:11 +0100

    :sparkles: Add github start button

--- src/SideMenu.elm
+++ src/SideMenu.elm
@@ -3,11 +3,13 @@ module SideMenu exposing (view)
 import Calendar.Msg exposing (TimeSpan(..))
 import Config exposing (allGroups)
 import Html exposing (Html, a, button, div, i, input, label, option, select, span, text)
-import Html.Attributes exposing (attribute, checked, class, for, id, multiple, style, title, type_, value)
+import Html.Attributes exposing (attribute, checked, class, for, href, id, multiple, style, target, title, type_, value)
 import Html.Events exposing (on, onCheck, onClick, targetValue)
 import Json.Decode as Json
 import Model exposing (Group, Settings)
 import Msg exposing (Msg(..))
+import Svg exposing (path, svg)
+import Svg.Attributes exposing (d, fillRule, height, version, viewBox, width)
 import Utils exposing (groupId)
 
 
@@ -36,8 +38,13 @@ view group timespan { menuOpened, showCustom, showHack2g2 } =
             [ class "sidemenu--footer" ]
             [ div
                 [ class "sidemenu--footer-content" ]
-                [ a [] [ i [ class "icon-secure" ] [] ]
-                , span [] [ text "Secured by CP" ]
+                [ div [ class "sidemenu--row" ] [ githubButton ]
+                , div
+                    [ class "sidemenu--row" ]
+                    [ a [] [ i [ class "icon-secure" ] [] ]
+                    , span [] [ text "Secured by" ]
+                    , span [ title "CyberPlanning", style "margin-left" "3px" ] [ text "CP" ]
+                    ]
                 ]
             ]
         ]
@@ -85,4 +92,18 @@ modeButton : TimeSpan -> String -> Html Msg
 modeButton mode name =
     div []
         [ button [ onClick (ChangeMode mode), attribute "aria-label" ("Toggle Mode " ++ name) ] [ text name ]
+        ]
+
+
+githubButton : Html msg
+githubButton =
+    a
+        [ class "sidemenu--github-btn", href "https://github.com/cyberplanning/webclient", target "_blank" ]
+        [ svg
+            [ version "1.1", width "16", height "16", viewBox "0 0 16 16" ]
+            [ path
+                [ fillRule "evenodd", d "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z" ]
+                []
+            ]
+        , span [] [ text "Star" ]
         ]
